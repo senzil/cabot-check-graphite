562dee66ae9cc6956affde3b55daa8318f2a7817
Jonathan Balls <j.balls@btopenworld.com>
2016-05-11 08:19:29 +0100

    All tests ported and working

--- cabot_check_graphite/plugin.py
+++ cabot_check_graphite/plugin.py
@@ -4,21 +4,48 @@ from django.core.urlresolvers import reverse
 from django import forms
 from django.template import Context, Template
 
-from cabot.plugins.models import CheckPlugin
-from cabot.plugins.forms import CheckConfigForm
+from cabot.plugins.models import StatusCheckPlugin
 from cabot.cabotapp.models import StatusCheckResult
 
 from os import environ as env
 import subprocess
 import requests
 import logging
+import itertools
+from .graphite import parse_metric, get_data
+
+import logging
+logger = logging.getLogger(__name__)
+
+CHECK_TYPES = (
+    ('>', 'Greater than'),
+    ('>=', 'Greater than or equal'),
+    ('<', 'Less than'),
+    ('<=', 'Less than or equal'),
+    ('==', 'Equal to'),
+)
+
+class GraphiteStatusCheckForm(forms.Form):
+    metric = forms.CharField(
+        help_text='fully.qualified.name of the Graphite metric you want to watch. This can be any valid Graphite expression, including wildcards, multiple hosts, etc.',
+    )
+    check_type = forms.ChoiceField(
+        choices=CHECK_TYPES,
+    )
+    value = forms.IntegerField(
+        help_text='If this expression evaluates to true, the check will fail (possibly triggering an alert).',
+    )
+    expected_num_hosts = forms.IntegerField(
+        initial=0,
+        help_text='The minimum number of data series (hosts) you expect to see.',
+    )
+    allowed_num_failures = forms.IntegerField(
+        initial=0,
+        help_text='The maximum number of data series (metrics) you expect to fail. For example, you might be OK with 2 out of 3 webservers having OK load (1 failing), but not 1 out of 3 (2 failing).',
+    )
 
 
-class GraphiteStatusCheckForm(CheckConfigForm):
-    name = forms.CharField(max_length=80)
-
-
-class GraphiteStatusCheckPlugin(CheckPlugin):
+class GraphiteStatusCheckPlugin(StatusCheckPlugin):
     name = "Graphite"
     slug = "graphite-check"
     author = "Jonathan Balls"
@@ -27,43 +54,101 @@ class GraphiteStatusCheckPlugin(CheckPlugin):
 
     config_form = GraphiteStatusCheckForm
 
-    def target(self, check):
-        instances = check.instance_set.all()
-        services = check.service_set.all()
-        if instances:
-            return instances[0]
-        elif services:
-            return services[0]
+    def format_error_message(self, check, failures, actual_hosts, hosts_by_target):
+        if actual_hosts < check.expected_num_hosts:
+            return "Hosts missing | %d/%d hosts" % (
+                actual_hosts, check.expected_num_hosts)
+        elif actual_hosts > 1:
+            threshold = float(check.value)
+            failures_by_host = ["%s: %s %s %0.1f" % (
+                hosts_by_target[target], value, check.check_type, threshold)
+                for target, value in failures]
+            return ", ".join(failures_by_host)
         else:
-            return None
-    
-    def run(self, check):
-        result = StatusCheckResult(status_check=check)
-        instances = check.instance_set.all()
-        services = check.service_set.all()
-        
-        if instances:
-            target = instances[0].address
-        else:
-            target = services[0].url
+            target, value = failures[0]
+            return "%s %s %0.1f" % (value, check.check_type, float(check.value))
 
-        # We need to read both STDOUT and STDERR because ping can write to both, depending on the kind of error. Thanks a lot, ping.
-        ping_process = subprocess.Popen("ping -c 1 " + target, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-        response = ping_process.wait()
+    def run(self, check, result):
 
-        if response == 0:
-            result.succeeded = True
-        else:
-            output = ping_process.stdout.read()
+        failures = []
+        graphite_output = parse_metric(check.metric, mins_to_check=check.frequency)
+
+        try:
+            result.raw_data = json.dumps(graphite_output['raw'])
+        except:
+            result.raw_data = graphite_output['raw']
+
+        if graphite_output["error"]:
             result.succeeded = False
-            result.error = output
+            result.error = graphite_output["error"]
+            return result
+
+        if graphite_output['num_series_with_data'] > 0:
+            result.average_value = graphite_output['average_value']
+            for s in graphite_output['series']:
+                if not s["values"]:
+                    continue
+                failure_value = None
+                if check.check_type == '<':
+                    if float(s['min']) < float(check.value):
+                        failure_value = s['min']
+                elif check.check_type == '<=':
+                    if float(s['min']) <= float(check.value):
+                        failure_value = s['min']
+                elif check.check_type == '>':
+                    if float(s['max']) > float(check.value):
+                        failure_value = s['max']
+                elif check.check_type == '>=':
+                    if float(s['max']) >= float(check.value):
+                        failure_value = s['max']
+                elif check.check_type == '==':
+                    if float(check.value) in s['values']:
+                        failure_value = float(check.value)
+                else:
+                    raise Exception(u'Check type %s not supported' %
+                                    check.check_type)
+
+                if not failure_value is None:
+                    failures.append((s["target"], failure_value))
+
+        if len(failures) > check.allowed_num_failures:
+            result.succeeded = False
+        elif graphite_output['num_series_with_data'] < check.expected_num_hosts:
+            result.succeeded = False
+        else:
+            result.succeeded = True
+
+        if not result.succeeded:
+            targets = [s["target"] for s in graphite_output["series"]]
+            hosts = self.minimize_targets(targets)
+            hosts_by_target = dict(zip(targets, hosts))
+
+            result.error = self.format_error_message(
+		check,
+                failures,
+                graphite_output['num_series_with_data'],
+                hosts_by_target,
+            )
 
         return result
 
-    def description(self, check):
-        target = self.target(check)
-        if target:
-            return 'ICMP Reply from {}'.format(target.name)
-        else:
-            return 'ICMP Check with no target.'
+    def minimize_targets(self, targets):
+	split = [target.split(".") for target in targets]
+
+	prefix_nodes_in_common = 0
+	for i, nodes in enumerate(itertools.izip(*split)):
+	    if any(node != nodes[0] for node in nodes):
+		prefix_nodes_in_common = i
+		break
+	split = [nodes[prefix_nodes_in_common:] for nodes in split]
+
+	suffix_nodes_in_common = 0
+	for i, nodes in enumerate(reversed(zip(*split))):
+	    if any(node != nodes[0] for node in nodes):
+		suffix_nodes_in_common = i
+		break
+	if suffix_nodes_in_common:
+	    split = [nodes[:-suffix_nodes_in_common] for nodes in split]
+
+	return [".".join(nodes) for nodes in split]
 
---
+++ cabot_check_graphite/tests.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.auth.models import User
+from cabot.cabotapp.tests.tests_basic import LocalTestCase
+from cabot.cabotapp.models import StatusCheck, Instance
+from cabot.plugins.models import StatusCheckPluginModel
+from cabot_check_graphite.plugin import GraphiteStatusCheckPlugin
+from cabot.cabotapp.models import Service, StatusCheckResult
+from mock import Mock, patch
+import os
+from .graphite import parse_metric
+import json
+import time
+
+from logging import getLogger
+logger = getLogger(__name__)
+
+def get_content(fname):
+    path = os.path.join(os.path.dirname(__file__), 'fixtures/%s' % fname)
+    with open(path) as f:
+        return f.read()
+
+def fake_graphite_response(*args, **kwargs):
+    resp = Mock()
+    resp.json = lambda: json.loads(get_content('graphite_response.json'))
+    resp.status_code = 200
+    return resp
+
+
+def fake_graphite_series_response(*args, **kwargs):
+    resp = Mock()
+    resp.json = lambda: json.loads(get_content('graphite_avg_response.json'))
+    resp.status_code = 200
+    return resp
+
+
+def fake_empty_graphite_response(*args, **kwargs):
+    resp = Mock()
+    resp.json = lambda: json.loads(get_content('graphite_null_response.json'))
+    resp.status_code = 200
+    return resp
+
+
+def fake_slow_graphite_response(*args, **kwargs):
+    resp = Mock()
+    time.sleep(0.1)
+    resp.json = lambda: json.loads(get_content('graphite_null_response.json'))
+    resp.status_code = 200
+    return resp
+
+class TestGraphiteCheckCheckPlugin(LocalTestCase):
+
+    def setUp(self):
+        super(TestGraphiteCheckCheckPlugin, self).setUp()
+
+        self.graphite_check_model, created = StatusCheckPluginModel.objects.get_or_create(
+	    slug='graphite-check'
+	    )
+
+        self.graphite_check = StatusCheck.objects.create(
+            check_plugin=self.graphite_check_model,
+            name='Graphite Check',
+            metric='stats.fake.value',
+            check_type='>',
+            value='9.0',
+            created_by=self.user,
+            importance=Service.ERROR_STATUS,
+        )
+
+        self.graphite_check.save()
+        self.graphite_check = StatusCheck.objects.get(pk=self.graphite_check.pk)
+	self.service.status_checks.add(self.graphite_check)
+
+    @patch('cabot_check_graphite.graphite.requests.get', fake_graphite_response)
+    def test_graphite_run(self):
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 0)
+        self.graphite_check.run()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 1)
+        # Most recent check failed
+        self.assertFalse(self.graphite_check.last_result().succeeded)
+        self.assertEqual(self.graphite_check.calculated_status,
+                         Service.CALCULATED_FAILING_STATUS)
+        # This should now pass
+        self.graphite_check.value = '11.0'
+        self.graphite_check.save()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 1)
+        self.graphite_check.run()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 2)
+        self.assertEqual(self.graphite_check.calculated_status,
+                         Service.CALCULATED_PASSING_STATUS)
+        # As should this - passing but failures allowed
+        self.graphite_check.allowed_num_failures = 2
+        self.graphite_check.save()
+        self.graphite_check.run()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 3)
+        self.assertEqual(self.graphite_check.calculated_status,
+                         Service.CALCULATED_PASSING_STATUS)
+        # As should this - failing but 1 failure allowed
+        # (in test data, one data series is entirely below 9 and one goes above)
+        self.graphite_check.value = '9.0'
+        self.graphite_check.allowed_num_failures = 1
+        self.graphite_check.save()
+        self.graphite_check.run()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 4)
+        self.assertEqual(self.graphite_check.calculated_status,
+                         Service.CALCULATED_PASSING_STATUS,
+                         list(checkresults)[-1].error)
+        # And it will fail if we don't allow failures
+        self.graphite_check.allowed_num_failures = 0
+        self.graphite_check.save()
+        self.graphite_check.run()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 5)
+        self.assertEqual(self.graphite_check.calculated_status,
+                         Service.CALCULATED_FAILING_STATUS)
+        result = checkresults.order_by('-time')[0]
+        self.assertEqual(result.error, u'PROD: 9.16092 > 9.0')
+
+    @patch('cabot_check_graphite.graphite.requests.get', fake_graphite_series_response)
+    def test_graphite_series_run(self):
+        jsn = parse_metric('fake.pattern')
+        self.assertEqual(jsn['average_value'], 59.86)
+        self.assertEqual(jsn['series'][0]['max'], 151.0)
+        self.assertEqual(jsn['series'][0]['min'], 0.1)
+
+    @patch('cabot_check_graphite.graphite.requests.get', fake_empty_graphite_response)
+    def test_graphite_empty_run(self):
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 0)
+        self.graphite_check.run()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 1)
+        self.assertTrue(self.graphite_check.last_result().succeeded)
+        self.assertEqual(self.graphite_check.calculated_status,
+                         Service.CALCULATED_PASSING_STATUS)
+        self.graphite_check.expected_num_hosts = 1
+        self.graphite_check.save()
+        self.graphite_check.run()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 2)
+        self.assertFalse(self.graphite_check.last_result().succeeded)
+        self.assertEqual(self.graphite_check.calculated_status,
+                         Service.CALCULATED_FAILING_STATUS)
+
+    @patch('cabot_check_graphite.graphite.requests.get', fake_slow_graphite_response)
+    def test_graphite_timing(self):
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 0)
+        self.graphite_check.run()
+        checkresults = self.graphite_check.statuscheckresult_set.all()
+        self.assertEqual(len(checkresults), 1)
+        self.assertTrue(self.graphite_check.last_result().succeeded)
+        self.assertGreater(list(checkresults)[-1].took, 0.0)
+
